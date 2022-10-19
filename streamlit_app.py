f463700357c6cc2d4e389c28a066823e5a4eb340
premk83 <49036171+premk83@users.noreply.github.com>
2022-10-20 00:41:47 +0530

    Update streamlit_app.py
    Adjusted indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,10 +26,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # Create a repeatable code block (function)
-  def get_fruityvice_data(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
+    return fruityvice_normalized
 
 # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
